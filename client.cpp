ec881a7fda68a2edbc3b23534ebd230f4a3ac423
dav1dvorobev <dav1dvorobev@vk.com>
2023-11-18 20:36:37 +0300

    sending preload_history as a number and using the updated readString

--- client.cpp
+++ client.cpp
@@ -4,51 +4,54 @@
 
 using json = nlohmann::json;
 
-std::string __buffer;
+std::string buf;
 void readFromServer(SocketShell clientSocket){
     while(true){
-        std::string message;
-        if(readString(clientSocket, message) == 0){break;}
+        std::string message = readString(clientSocket);
         std::cout << CLEAR_LINE << message;
-        if(!__buffer.empty()){
-            std::cout << __buffer;
+        if(!buf.empty()){
+            std::cout << buf;
             std::cout.flush();
         }
     }
 }
 int main(int argc, char** argv){
-    std::ifstream __file("config.json");
-    if(!__file){
+    std::ifstream file("config.json");
+    if(!file){
         std::cerr << "Cannot opening file" << std::endl;
         return -1;
     }
-    json config = json::parse(__file);
-    std::string __address = config["server_address"];
-    const int __port = config["port"];
+    json config = json::parse(file);
+    const std::string address = config["server_address"];
+    const int port = config["port"];
+    const int preload_history = config["preload_history"];
     try{
-        SocketShell clientSocket(__address, __port);
+        SocketShell clientSocket(address, port);
         while(true){
             std::cout << "login: ";
             sendString(clientSocket, input());
             std::cout << "password: ";
             sendString(clientSocket, input("hidden"));
-            readString(clientSocket, __buffer);
-            if(__buffer == "accept"){break;}
-            else{std::cout << RED + __buffer + RESET << std::endl;}
+            buf = readString(clientSocket);
+            if(buf == "accept"){
+                send(clientSocket, &preload_history, sizeof(int), 0);
+                break;
+            }
+            else{std::cout << buf << std::endl;}
         }
-        sendString(clientSocket, config["preload_msgs"]);
         std::thread readThread(readFromServer, clientSocket);
         readThread.detach();
         while(true){
-            input(__buffer);
-            if(__buffer == "!q"){break;}
+            input(buf);
+            if(buf == "!q"){break;}
             std::cout << UP;
-            if(validation(__buffer)){sendString(clientSocket, __buffer);}
+            if(validation(buf)){sendString(clientSocket, buf);}
             else{std::cout << CLEAR_LINE << RED << "validation error" << RESET << ENDL;}
         }
-        closeSocket(clientSocket);
+        close(clientSocket);
     }catch(std::exception& e){
         logging(e.what(), config["logging_path"]);
+        throw;
     }
     return 0;
 }
